d7d3b4ff5387812762d16fb0329d71b3f946dfce
1024-byteeeee <2270484921@qq.com>
2025-05-23 22:01:42 +0800

    :white_check_mark: GNU Lesser General Public License v3.0

--- src/main/java/top/byteeeee/fuzz/settings/Rule.java
+++ src/main/java/top/byteeeee/fuzz/settings/Rule.java
@@ -1,21 +1,21 @@
 /*
- * This file is part of the KKK project, licensed under the
+ * This file is part of the Fuzz project, licensed under the
  * GNU Lesser General Public License v3.0
  *
  * Copyright (C) 2025 1024_byteeeee and contributors
  *
- * KKK is free software: you can redistribute it and/or modify
+ * Fuzz is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * KKK is distributed in the hope that it will be useful,
+ * Fuzz is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with KKK. If not, see <https://www.gnu.org/licenses/>.
+ * along with Fuzz. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package top.byteeeee.fuzz.settings;
