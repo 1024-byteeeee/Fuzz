facee701af980e8a239b948bc930fc979a6f3ae2
1024_byteeeee <2270484921@qq.com>
2025-12-21 16:07:45 +0800

    补充1.21.11修改颜色的几个规则

---
+++ src/main/java/top/byteeeee/fuzz/mixin/rule/waterColor/WaterRendererMixin.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the Fuzz project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2025 1024_byteeeee and contributors
+ *
+ * Fuzz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fuzz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fuzz. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package top.byteeeee.fuzz.mixin.rule.waterColor;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import top.byteeeee.annotationtoolbox.annotation.GameVersion;
+
+import top.byteeeee.fuzz.utils.compat.DummyClass;
+
+@Environment(EnvType.CLIENT)
+@GameVersion(version = "Minecraft >= 1.21.11")
+@Mixin(DummyClass.class)
+public abstract class WaterRendererMixin {}
