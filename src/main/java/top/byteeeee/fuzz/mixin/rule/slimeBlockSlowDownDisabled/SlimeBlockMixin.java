e4b651ba75fd59d2d6d4b66a59850a6be27a6c89
1024_byteeeee <2270484921@qq.com>
2026-07-16 15:08:13 +0800

    修复已知问题

--- src/main/java/top/byteeeee/fuzz/mixin/rule/slimeBlockSlowDownDisabled/SlimeBlockMixin.java
+++ src/main/java/top/byteeeee/fuzz/mixin/rule/slimeBlockSlowDownDisabled/SlimeBlockMixin.java
@@ -1,30 +1,83 @@
+/*
+ * This file is part of the Fuzz project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2026 1024_byteeeee and contributors
+ *
+ * Fuzz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fuzz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fuzz. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.byteeeee.fuzz.mixin.rule.slimeBlockSlowDownDisabled;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 import net.minecraft.block.SlimeBlock;
-import net.minecraft.block.TransparentBlock;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import top.byteeeee.fuzz.FuzzSettings;
 import top.byteeeee.fuzz.utils.ClientUtil;
 
 @Environment(EnvType.CLIENT)
 @Mixin(SlimeBlock.class)
-public abstract class SlimeBlockMixin extends TransparentBlock {
-    protected SlimeBlockMixin(Settings settings) {
-        super(settings);
+public abstract class SlimeBlockMixin {
+    @ModifyExpressionValue(
+        method = "onSteppedOn",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;bypassesSteppingEffects()Z"
+        )
+    )
+    private boolean skipOnSteppedOnLogic(boolean original) {
+        if (FuzzSettings.slimeBlockSlowDownDisabled && ClientUtil.isLocalPlayerTicking()) {
+            return true;
+        } else {
+            return original;
+        }
     }
 
-    @Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
-    private void skipOnSteppedOnLogic(CallbackInfo ci) {
+    @ModifyExpressionValue(
+        method = "onLandedUpon",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;bypassesLandingEffects()Z"
+        )
+    )
+    private boolean skipOnLandedUponLogic(boolean original) {
         if (FuzzSettings.slimeBlockSlowDownDisabled && ClientUtil.isLocalPlayerTicking()) {
-            ci.cancel();
+            return true;
+        } else {
+            return original;
+        }
+    }
+
+    @ModifyExpressionValue(
+        method = "onEntityLand",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;bypassesLandingEffects()Z"
+        )
+    )
+    private boolean skipOnEntityLandLogic(boolean original) {
+        if (FuzzSettings.slimeBlockSlowDownDisabled && ClientUtil.isLocalPlayerTicking()) {
+            return true;
+        } else {
+            return original;
         }
     }
 }
